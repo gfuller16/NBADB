055cb2ae3ace42e69cef9a3293ea7c9eea18ed56
gfuller16 <graysonfuller16@gmail.com>
2018-03-06 08:29:30 -0500

    Update spTripleDoubleClub.sql

--- spTripleDoubleClub.sql
+++ spTripleDoubleClub.sql
@@ -1,39 +1,24 @@
 USE [NBADB]
 GO
-/****** Object:  StoredProcedure [dbo].[spTripleDoubleClub]    Script Date: 4/24/2017 4:17:37 PM ******/
+/****** Object:  StoredProcedure [dbo].[spTripleDoubleClub]    Script Date: 3/6/2018 8:29:07 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE PROCEDURE [dbo].[spTripleDoubleClub]
+ALTER PROCEDURE [dbo].[spTripleDoubleClub]
 AS
 BEGIN
 
-	;WITH cte_Averages AS
-	(
-	SELECT CONVERT(VARCHAR(30),p.plFirstName + ' ' + ISNULL(p.plLastName,'')) as [Player Name]
-		  ,CONVERT(VARCHAR(17),t.tmLocation) AS [Location]
-		  ,po.[poShortDesc] AS [Position]
-		  ,CONVERT(DECIMAL(4,1),AVG(CONVERT(DECIMAL(4,1),[gsPoints]))) as AvgPoints
-		  ,CONVERT(DECIMAL(4,1),AVG(CONVERT(DECIMAL(4,1),[gsAssists]))) as AvgAssists
-		  ,CONVERT(DECIMAL(4,1),AVG(CONVERT(DECIMAL(4,1),[gsRebounds]))) as AvgRebounds
-		  ,COUNT([gs_gmID]) as GamesPlayed
-	  FROM [NBADB].[dbo].[tblGameStats] gs
-	  JOIN tblPlayer p
-		ON p.plID = gs.gs_plID
-	  JOIN tblTeam t
-		ON t.tmID = p.plTeam
-	  JOIN tblPosition po
-		ON po.poID = p.plPosition
-	GROUP BY p.plFirstName,p.plLastName,t.tmLocation,po.[poShortDesc]
-	)
-
 	SELECT
-	*
-	FROM cte_Averages
-	WHERE AvgPoints >= 10.0
-	AND AvgAssists >= 10.0
-	AND AvgRebounds >= 10.0
-	ORDER BY AvgPoints + AvgAssists + AvgRebounds DESC
+	p.plFirstName + ' ' + p.plLastName as [Player],
+	td.tdPoints as [Points],
+	td.tdAssists as [Assists],
+	td.tdRebounds as [Rebounds],
+	td.tdYear as [Year]
+	FROM tblTripleDoubleClub td
+	JOIN tblPlayer p
+		ON p.plID = td.tdPlayerID_plID
+	ORDER BY tdYear,tdPoints,tdAssists,tdRebounds
+
 END
